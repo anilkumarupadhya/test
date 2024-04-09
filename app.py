096fd03ea04c0b1cb1a2dd6859ba7cb2ef1769a6
anilkumarupadhya <90567416+anilkumarupadhya@users.noreply.github.com>
2024-04-09 14:13:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-for i in range (0, 3):
-    print("jai shri ram")
-    
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for your Flask application
+@app.route('/')
+def index():
+    output = ""
+    for i in range(0, 3):
+        output += "jai shri ram<br>"
+    return output
+
+# Run the Flask application
+if __name__ == '__main__':
+    app.run(debug=True)
